3ea46d6fd8ffd26e44c6a03c8b5d1b947bd3c9c1
Marat G <maratoid@gmail.com>
2015-11-18 10:06:12 -0800

    first commit

---
+++ terraform-provider-coreosbox.rb
@@ -0,0 +1,11 @@
+class TerraformProviderCoreosbox < Formula
+  desc "Terraform provider for getting CoreOS box information for a given IaaS, update channel and version (or current version)"
+  homepage "https://github.com/Samsung-AG/terraform-provider-coreosbox"
+  url "https://github.com/Samsung-AG/terraform-provider-coreosbox/releases/download/v0.0.1/terraform-provider-coreosbox_darwin_amd64.tar.gz"
+  version "0.0.1"
+  sha256 "666ff6adbd56e1d86562685365c8de4362a84793eb38824a67953e87775fb7bb"
+
+  def install
+    bin.install "terraform-provider-coreosbox"
+  end
+end
