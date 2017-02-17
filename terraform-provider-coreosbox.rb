70ade9974c4f2bf19b937df84e1d94e23d650b04
Patrick Christopher <coffeepac@gmail.com>
2017-02-17 12:07:15 -0800

    update to use version 0.3.0 of provider
    update local version to 0.0.3 and sha hash

--- terraform-provider-coreosbox.rb
+++ terraform-provider-coreosbox.rb
@@ -1,9 +1,9 @@
 class TerraformProviderCoreosbox < Formula
   desc 'Terraform provider for getting CoreOS box information for a given IaaS, update channel and version (or current version)'
   homepage 'https://github.com/samsung-cnct/terraform-provider-coreosbox'
-  url 'https://github.com/samsung-cnct/terraform-provider-coreosbox/releases/download/v0.0.2/terraform-provider-coreosbox_darwin_amd64.tar.gz'
-  version '0.0.2'
-  sha256 'fdaafea86561e213d80fb0bf1dd4f051fc03c0409757d6eb5aac67b2e24b8908'
+  url 'https://github.com/samsung-cnct/terraform-provider-coreosbox/releases/download/v0.0.3/terraform-provider-coreosbox_darwin_amd64.tar.gz'
+  version '0.0.3'
+  sha256 '95cbe7cd329094736d91cd51a8c6372653c28f3997c511b3db89e49a1d280ecf'
   depends_on 'terraform'
 
 
